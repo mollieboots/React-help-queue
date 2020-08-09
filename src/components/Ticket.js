8d0933b1bc27b7d3a980853e5d2182150b3232d1
apul8377 <apulvers@my.chemeketa.edu>
2020-08-09 14:44:58 -0700

    default setup and help-queue with react

---
+++ src/components/Ticket.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Ticket(props) {
+  return (
+    <React.Fragment>
+      <h3>
+        {props.location} - {props.names}
+      </h3>
+      <p>
+        <em>{props.issue}</em>
+      </p>
+    </React.Fragment>
+  );
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string,
+  location: PropTypes.string,
+  issue: PropTypes.string,
+};
+
+export default Ticket;
